ddc4644f1b667de9b2ff5c2135c887c6149eb6eb
yashh-143 <138308799+yashh-143@users.noreply.github.com>
2026-05-02 12:30:24 +0530

    Update canva_resume_builder_vite.config (1).js

--- canva_resume_builder_vite.config (1).js
+++ canva_resume_builder_vite.config (1).js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/website/', // This MUST match your repo name exactly
+  base: '/website/', // Ensure this is exactly your repo name with slashes
 })
-
-
-
